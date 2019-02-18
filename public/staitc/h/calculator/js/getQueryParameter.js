c24031f8258c74cbb42f95a9c3f94c417f6d37cf
zhaochenmin <zhaochenmin@dpandora.cn>
2019-02-18 19:02:27 +0800

    线上修复

--- public/staitc/h/calculator/js/getQueryParameter.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-function getQueryParameter (name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-  var params = window.location.search.substr(window.location.search.indexOf("?")+1)
-  var r = params.match(reg)
-  if (r != null)
-    return decodeURIComponent(r[2]);
-  return "";
-}

25364e29c56a6a1f5f4024baa8308d65e988b1fb
zhaochenmin <zhaochenmin@dpandora.cn>
2019-04-04 15:32:09 +0800

    新加layout

--- src/views/common/differPlatform/index.vue
+++ src/views/common/differPlatform/index.vue
@@ -1,43 +1,31 @@
+<!--
+  相当于项目根目录index.vue，进入从这里开始分流
+-->
 <template>
   <div class="index"></div>
 </template>
 
 <script>
+import { showFlag } from '@/api/common/common'
+
 export default {
-  name: 'index',
-  mixins: [],
-  components: {},
-  data() {
-    return {
-      msg: 'index'
-    }
-  },
-  props: {},
-  watch: {},
-  methods: {},
-  computed: {},
+  name: 'differPlatform',
   created() {
     showFlag().then(res => {
       const data = res.data
       if (res.data.resultCode === '1') {
-        this.setPlatform(data.data.platformFlag === '1' ? 'djs' : 'hyc')
-        // switch (data.data.platformFlag) {
-        //   case '1':
-        //     // 点金石
-        //     this.$router.push({ name: 'DJSHomePage' })
-        //     break
-        //   case '2':
-        //     // 汇有财
-        //     this.$router.push({ name: 'HYCHomePage' })
-        //     break
-        // }
+        switch (data.data.platformFlag) {
+          case '1':
+            // 点金石
+            this.$router.push({ name: 'DJSHomePage' })
+            break
+          case '2':
+            // 汇有财
+            this.$router.push({ name: 'HYCHomePage' })
+            break
+        }
       }
     })
-  },
-  mounted() {},
-  destroyed() {}
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
--- src/views/djs/homePage/index.vue
+++ src/views/djs/homePage/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="index">
-    hello, {{msg}}
+    <header>
+      <h6>汇有财</h6>
+      <p>唯有赚钱不能停</p>
+      <button :class="{new: newNotice}"></button>
+    </header>
+    <ul class="tabs">
+      <li>信息披露</li>
+      <li>资金存管</li>
+      <li>运营数据</li>
+      <li>活动推荐</li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +21,7 @@ export default {
   components: {},
   data() {
     return {
-      msg: 'djs-homepage'
+      newNotice: true // 是否有新公告
     }
   },
   props: {},
@@ -25,4 +35,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../../assets/css/mixins';
+@import '../../../assets/css/theme';
+
+.index {
+  header {
+    position: relative;
+    h6 {
+      font-size: 0.24rem;
+      color: $color-text-b;
+      text-align: center;
+      line-height: 0.44rem;
+      font-weight: bold;
+    }
+    p {
+      font-size: 0.14rem;
+      color: #999;
+      letter-spacing: 0.85em;
+      text-align: center;
+      margin-bottom: 0.15rem;
+    }
+    button {
+      position: absolute;
+      top: 0.1rem;
+      right: 0.15rem;
+      @include cube(0.25rem, 0.19rem);
+      background: url('./notice.png') center/contain no-repeat;
+      &.new {
+        background-image: url('./notice-new.png');
+      }
+    }
+  }
+  .tabs {
+    display: flex;
+    justify-content: space-around;
+  }
+}
 </style>

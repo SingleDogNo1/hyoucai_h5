c01425ef848d4066ada8c6b0f80fac14318dc211
shuxing <wsx12345654321>
2019-04-16 10:55:42 +0800

    加息券消息详情

---
+++ src/views/djs/couponMessageList/couponMsgDetail.vue
@@ -0,0 +1,67 @@
+<template>
+  <section>
+    <div class="msg">
+      <p><em>5.0%加息券</em></p>
+      <p><i>已存入您的账户，立即使用加息30天</i></p>
+    </div>
+  </section>
+</template>
+
+<script>
+// import api from '@/api/djs/message'
+// import BScroll from '@/components/BScroll/BScroll'
+
+export default {
+  name: 'index',
+  mixins: [],
+  components: {
+    // BScroll
+  },
+  data() {
+    return {}
+  },
+  props: {},
+  watch: {},
+  methods: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  destroyed() {}
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+  background: #eee;
+  padding-top: 0.06rem;
+  height: 100%;
+  .msg {
+    height: 1.11rem;
+    background-image: url(./couponMsgBg.png);
+    background-repeat: no-repeat;
+    background-position: 0.1rem;
+    margin-right: 0.1rem;
+    margin-bottom: 0.15rem;
+    p {
+      em {
+        color: #fff;
+        font-size: 0.18rem;
+        font-weight: bold;
+        font-style: normal;
+        display: block;
+        text-align: center;
+        box-shadow: 0.02rem 0 0 #dbd624;
+        padding-top: 0.3rem;
+      }
+      i {
+        color: #fff;
+        font-size: 0.12rem;
+        font-style: normal;
+        display: block;
+        text-align: center;
+        padding-top: 0.05rem;
+      }
+    }
+  }
+}
+</style>

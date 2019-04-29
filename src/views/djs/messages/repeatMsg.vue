98d745af9438eb6dc04958f6bf8139a9df3275d6
shuxing <wsx12345654321>
2019-04-29 16:03:10 +0800

    点金石--消息中心-复投消息

--- src/views/djs/messages/repeatMsg.vue
+++ src/views/djs/messages/repeatMsg.vue
@@ -1,35 +1,53 @@
 <template>
-  <section v-if="datas.length > 0">
-    <div class="commonList" v-for="(item, index) in datas" :key="index" :class="item.readStatus == 1 ? 'isRead' : ''">
-      <div class="title" @click="toRepeatDetail(item.id, item.invOverDate)">
-        <p>{{ item.msg }}</p>
-      </div>
-      <div class="more"><img src="./more_icon.png" /></div>
+  <BScroll
+    class="message_wrap"
+    ref="messageWrapper"
+    :probeType="BScrollOption.probeType"
+    :listen-scroll="BScrollOption.listenScroll"
+    :pullup="BScrollOption.pullup"
+    @scrollToEnd="scrollToEnd"
+  >
+    <div>
+      <section v-if="datas.length > 0">
+        <div class="commonList" v-for="(item, index) in datas" :key="index" :class="item.readStatus == 1 ? 'isRead' : ''">
+          <div class="title" @click="toRepeatDetail(item.id, item.invOverDate)">
+            <p>{{ item.msg }}</p>
+          </div>
+          <div class="more"><img src="./more_icon.png" /></div>
+        </div>
+      </section>
+      <NoData v-else class="noData">
+        <p><img alt="" src="./noData.png" /></p>
+        <p>暂无消息</p>
+      </NoData>
     </div>
-  </section>
-  <NoData v-else class="noData">
-    <p><img alt="" src="./noData.png" /></p>
-    <p>暂无消息</p>
-  </NoData>
+  </BScroll>
 </template>
 
 <script>
+import BScroll from '@/components/BScroll/BScroll'
+import { Toast, Indicator } from 'mint-ui'
+import { mapGetters } from 'vuex'
 import api from '@/api/djs/message'
-import { getUser } from '@/assets/js/cache'
-import { getAuth } from '@/assets/js/utils'
 import NoData from '@/components/NoData/NoData'
 export default {
   name: 'index',
   mixins: [],
   components: {
+    BScroll,
     NoData
   },
   data() {
     return {
-      userName: getUser().userName,
-      authorization: getAuth(),
       repeatShow: false, //是否已读
-      datas: [] //列表
+      datas: [], //列表
+      BScrollOption: {
+        probeType: 3,
+        listenScroll: true,
+        pullup: true
+      },
+      page: 1,
+      hasMore: true
     }
   },
   props: {},
@@ -49,22 +67,47 @@ export default {
           query: { invOverDate }
         })
       })
+    },
+    getData() {
+      Indicator.open()
+      api
+        .getRepeatMessage({
+          userName: this.user.userName,
+          curPage: this.page
+        })
+        .then(res => {
+          Indicator.close()
+          let data = res.data.message
+          let repeatUnReadData = data.repeatUnRead
+          let repeatReadData = data.repeatRead
+          let list = repeatReadData.concat(repeatUnReadData)
+          let curPage = res.data.curPage
+          let countPage = res.data.countPag
+
+          if (!list.length) {
+            this.hasMore = false
+            Toast('没有更多数据了')
+          } else {
+            this.hasMore = parseInt(curPage) <= parseInt(countPage)
+          }
+          this.datas = [...this.datas, ...list]
+          this.$nextTick(() => {
+            this.$refs.messageWrapper.refresh()
+          })
+        })
+    },
+    scrollToEnd() {
+      if (this.hasMore) {
+        this.page++
+        this.getData()
+      }
     }
   },
-  computed: {},
+  computed: {
+    ...mapGetters(['user'])
+  },
   created() {
-    let data = {
-      userName: this.userName,
-      authorization: this.authorization
-    }
-    //复投消息接口
-    api.getRepeatMessage(data).then(res => {
-      let data = res.data.message
-      let repeatUnReadData = data.repeatUnRead
-      let repeatReadData = data.repeatRead
-
-      this.datas = repeatReadData.concat(repeatUnReadData)
-    })
+    this.getData()
   },
   mounted() {},
   destroyed() {}
@@ -72,60 +115,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-  background: #eee;
-  padding-top: 0.06rem;
+.message_wrap {
   height: 100%;
-  .commonList {
-    background: #fff;
-    height: 0.4rem;
-    line-height: 0.4rem;
-    border-bottom: 0.01rem solid #eeeeee;
-    font-size: 0.13rem;
-    color: #333333;
-    margin-bottom: 0.06rem;
-    border-radius: 0.05rem;
-    display: flex;
-    .title {
-      flex: 1;
-      width: 80%;
-      position: relative;
-      padding-left: 0.15rem;
-      p {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  > div {
+    section {
+      background: #eee;
+      padding-top: 0.06rem;
+      height: 100%;
+      .commonList {
+        background: #fff;
+        height: 0.4rem;
+        line-height: 0.4rem;
+        border-bottom: 0.01rem solid #eeeeee;
+        font-size: 0.13rem;
+        color: #333333;
+        margin-bottom: 0.06rem;
+        border-radius: 0.05rem;
+        display: flex;
+        .title {
+          flex: 1;
+          width: 80%;
+          position: relative;
+          padding-left: 0.15rem;
+          p {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+        }
+        .more {
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+          height: 0.4rem;
+          img {
+            height: 0.14rem;
+            width: 0.14rem;
+            padding-right: 0.15rem;
+          }
+        }
       }
-    }
-    .more {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      height: 0.4rem;
-      img {
-        height: 0.14rem;
-        width: 0.14rem;
-        padding-right: 0.15rem;
+      .isRead {
+        color: #ccc;
+        .title {
+          i {
+            background-color: #ccc;
+          }
+        }
       }
-    }
-  }
-  .isRead {
-    color: #ccc;
-    .title {
-      i {
-        background-color: #ccc;
+      .noData {
+        p {
+          text-align: center;
+          color: #999;
+          font-size: 0.15rem;
+        }
+        img {
+          margin-top: 0.5rem;
+          width: 35%;
+        }
       }
-    }
-  }
-  .noData {
-    p {
-      text-align: center;
-      color: #999;
-      font-size: 0.15rem;
-    }
-    img {
-      margin-top: 0.5rem;
-      width: 35%;
     }
   }
 }

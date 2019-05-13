3d6b42414e4a021ff5a64c754ea50a232208c66f
zhaochenmin <zhaochenmin@dpandora.cn>
2019-05-13 15:56:39 +0800

    标的利率改为用正常利率 + 活动利率 格式显示

--- src/views/djs/investDetail/easyLend.vue
+++ src/views/djs/investDetail/easyLend.vue
@@ -5,8 +5,11 @@
         <div class="huge">
           <ul>
             <li>
-              <span>{{ investDetail.investRate }}</span>
-              <span>%</span>
+              <span>{{ investDetail.basicsInvestRate }}</span>
+              <span class="pre">%</span>
+              <label v-if="investDetail.activityInvestRate">+</label>
+              <em class="act" v-if="investDetail.activityInvestRate">{{ investDetail.activityInvestRate }}</em>
+              <span class="pre" v-if="investDetail.activityInvestRate">%</span>
             </li>
             <li>
               <h5>{{ investDetail.projectName }}</h5>
@@ -473,9 +476,16 @@ export default {
           color: #ffffff;
           font-size: 0.15rem;
           span {
-            &:first-child {
-              font-size: 0.36rem;
+            font-size: 0.36rem;
+            &.pre {
+              font-size: 0.15rem;
             }
+          }
+          label {
+            font-size: 0.18rem;
+          }
+          em {
+            font-size: 0.18rem;
           }
           h5 {
             font-size: 0.15rem;
--- src/views/hyc/investDetail/easyLend.vue
+++ src/views/hyc/investDetail/easyLend.vue
@@ -5,8 +5,11 @@
         <div class="huge">
           <ul>
             <li>
-              <span>{{ projectInfo.investRate }}</span>
-              <span>%</span>
+              <span>{{ investDetail.basicsInvestRate }}</span>
+              <span class="pre">%</span>
+              <label v-if="investDetail.activityInvestRate">+</label>
+              <em class="act" v-if="investDetail.activityInvestRate">{{ investDetail.activityInvestRate }}</em>
+              <span class="pre" v-if="investDetail.activityInvestRate">%</span>
             </li>
             <li>
               <h5>{{ projectInfo.itemName }}</h5>
@@ -512,9 +515,16 @@ export default {
           color: #ffffff;
           font-size: 0.15rem;
           span {
-            &:first-child {
-              font-size: 0.36rem;
+            font-size: 0.36rem;
+            &.pre {
+              font-size: 0.15rem;
             }
+          }
+          label {
+            font-size: 0.18rem;
+          }
+          em {
+            font-size: 0.18rem;
           }
           h5 {
             font-size: 0.15rem;

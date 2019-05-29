61c842300202db218cf42ad5e0d9fbc074949379
shuxing <wsx12345654321>
2019-05-29 16:22:45 +0800

    交易记录bug修改

--- src/api/djs/transaction/index.js
+++ src/api/djs/transaction/index.js
@@ -1,10 +1,19 @@
 import request from '@/assets/js/requestDJS'
 import qs from 'qs'
 
-//交易记录
+//用户资金交易记录
 export function getUserTransaction(data) {
   return request({
     url: 'TrasactionList',
+    method: 'post',
+    data: qs.stringify(data)
+  })
+}
+
+//用户充值提现记录
+export function getTranByCzTxList(data) {
+  return request({
+    url: 'TranByCzTxList',
     method: 'post',
     data: qs.stringify(data)
   })

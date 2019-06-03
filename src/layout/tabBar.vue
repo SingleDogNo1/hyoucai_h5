d3ea2b8a47de44d8661d572288c377cf89f628de
zhaochenmin <zhaochenmin@dpandora.cn>
2019-06-03 10:47:15 +0800

    20190603 1st commit

--- src/layout/tabBar.vue
+++ src/layout/tabBar.vue
@@ -36,7 +36,13 @@ export default {
 @import '../assets/css/theme';
 
 .tab-bar {
-  height: 100vh;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
